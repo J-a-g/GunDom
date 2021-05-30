6e9c6ad3e618e4e4789647c51494bbf25d2db854
JAG <1019413106@qq.com>
2021-05-30 19:48:34 +0800

    添加菜单导航和抽屉式导航

--- app/src/main/java/com/yin/mvvmdemo/activity/BottomActivity.kt
+++ app/src/main/java/com/yin/mvvmdemo/activity/BottomActivity.kt
@@ -2,6 +2,7 @@ package com.yin.mvvmdemo.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import com.yin.mvvmdemo.R
@@ -13,12 +14,32 @@ class BottomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.acitivity_bottom)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+
         val host: NavHostFragment =
             supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment?
                 ?: return
 
         val navController = host.navController
-        findViewById<BottomNavigationView>(R.id.bottom_nav_view)?.setupWithNavController(navController)
+//        val appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBar(navController, appBarConfiguration)
+
+        setupBottomNavMenu(navController)
+    }
+
+    private fun setupActionBar(
+        navController: NavController,
+        appBarConfig: AppBarConfiguration
+    ) {
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        //底部菜单点击绑定
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav?.setupWithNavController(navController)
     }
 
 }
--- app/src/main/java/com/yin/mvvmdemo/activity/MenuActivity.kt
+++ app/src/main/java/com/yin/mvvmdemo/activity/MenuActivity.kt
@@ -1,12 +1,44 @@
 package com.yin.mvvmdemo.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.yin.mvvmdemo.R
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.acitivity_menu)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
+
+//        val navController = host.navController
+//        val appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBar(navController, appBarConfiguration)
+    }
+
+    private fun setupActionBar(navController: NavController,
+                               appBarConfig : AppBarConfiguration
+    ) {
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.my_nav_host_fragment))
+                || super.onOptionsItemSelected(item)
     }
 }
